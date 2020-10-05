d339bce09689d0e82deec8f2844a6a9e4faee154
Yolanda-HT <huitiandiao@gmail.com>
2020-10-05 14:56:15 -0400

    update

--- tools/pl_functions.R
+++ tools/pl_functions.R
@@ -106,6 +106,62 @@ conn_plot <- function(paga.pos.tb, paga.conn.tb, out_name_root, var_name = "NES"
   return(conn.plot)
 }
 
+# Node plot - pair wise comparison sample
+conn_plot_comp <- function(paga.pos.tb, paga.conn.tb, comparison, fillcol="grey", mute=TRUE) {
+  # paga.pos.tb: [x, y, Group]
+  # paga.conn.tb: [x1, y1, x2, y2, Conn]
+    out_name_root <- paste(comparison, collapse="_vs_")
+
+  paga.pos.tb.g1 <- paga.pos.tb %>% filter(Group == comparison[1])
+  paga.pos.tb.g2 <- paga.pos.tb %>% filter(Group == comparison[2])
+
+  # Calculate range of plotting
+  max_x <- max(paga.pos.tb$x)
+  min_x <- min(paga.pos.tb$x)
+  max_y <- max(paga.pos.tb$y)
+  min_y <- min(paga.pos.tb$y)
+  range_x <- max_x - min_x
+  range_y <- max_y - min_y
+  min_x <- min_x - range_x*0.1
+  max_x <- max_x + range_x*0.1
+  min_y <- min_y - range_y*0.1
+  max_y <- max_y + range_y*0.1
+  
+  conn.plot <- ggplot() +
+    geom_segment(data=paga.conn.tb, aes(x=x1, y=y1, xend=x2, yend=y2), size=paga.conn.tb$Conn*1.5, alpha=0.1) +
+    geom_point(data=paga.pos.tb, aes(x=x, y=y), color="grey", alpha=1) +
+    geom_point(data=paga.pos.tb.g1, aes(x=x, y=y), color="firebrick3", alpha=1) +
+    geom_point(data=paga.pos.tb.g2, aes(x=x, y=y), color="dodgerblue2", alpha=1) +
+    scale_x_continuous(limits = c(min_x, max_x)) +
+    scale_y_continuous(limits = c(min_y, max_y)) +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_rect(fill = fillcol),
+          axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.title.y = element_blank(),
+          axis.text.y = element_blank(),
+          axis.line.x = element_blank(), # element_line(color="black", size = 0.25)
+          axis.line.y = element_blank(), # element_line(color="black", size = 0.25)
+          axis.ticks = element_blank()
+         )
+  
+  if (!mute) {
+    conn.plot.nolegend <- conn.plot +
+      theme(legend.position="none")
+    out_name <- paste(out_name_root, ".pdf", sep="")
+    out_name_nolegend <- paste(out_name_root, "_nolegend.pdf", sep="")
+    out_name_nolegend_png <- paste(out_name_root, "_nolegend.png", sep="")
+    
+    ggsave(out_name, conn.plot, device="pdf", width=10, height=7.5, units=('cm'), dpi=300)
+    ggsave(out_name_nolegend, conn.plot.nolegend, device="pdf", width=4, height=4, units=('cm'), dpi=300)
+    ggsave(out_name_nolegend_png, conn.plot.nolegend, device="png", width=4, height=4, units=('cm'), dpi=300)
+  }
+  conn.plot <- conn.plot + ggtitle(out_name_root) + theme(plot.title = element_text(face="bold", hjust = 0.5))
+    
+  return(conn.plot)
+}
+
 
 ###----- Volcano plot
 volcano_plot <- function(comp_df, h_genes, log2fc_c, nlog10p_c) {
