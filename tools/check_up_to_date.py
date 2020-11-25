1e5d1ea6ae31692aefa8ba78f6e3900476bf31e2
YoloDHT <huitiandiao@gmail.com>
2020-11-25 13:02:21 -0500

    update

--- tools/check_up_to_date.py
+++ tools/check_up_to_date.py
@@ -3,9 +3,12 @@ import os
 import glob
 import sys
 
-print("----------")
+print("")
+print("")
+print("")
+print("--------------------------------------------------------------------------------")
 print("Check if files are up-to-date and move old versions into a 'old' folder")
-print("----------")
+print("--------------------------------------------------------------------------------")
 print("Usage:")
 print("python check_up-to-date.py path_to_folder_for_checking")
 print("----------")
@@ -32,13 +35,14 @@ files = glob.glob("*")
 files_simp = [x.split("_")[0] for x in files]
 files_simp = list(set(files_simp))
 
-
 for i in files_simp:
-    files_i = [x for x in files if "%s_"%i in x]
+    files_i = [x for x in files if "%s_"%i in x] + [x for x in files if "%s."%i in x]
+    use_date = ""
     if ("20" in "".join(files_i)): # Disregard files that do not have dates
         
+        files_i_dated = [x for x in files_i if "20" in x]
         # Get date list & find the most recent date
-        files_i_dates = [x.split(".")[-2].split("_")[-1].replace("new", "") for x in files_i]
+        files_i_dates = [x.split(".")[-2].split("_")[-1].replace("new", "") for x in files_i_dated]
         files_i_dates = [int(x) for x in list(set(files_i_dates))]
         files_i_dates.sort()
         use_date = str(files_i_dates[-1])
@@ -46,8 +50,9 @@ for i in files_simp:
             use_date = "%snew"%use_date
         print("%s:\t%s"%(i,use_date))
         
-        # Move files that are not most recent dates into 'old' folder
+    # Move files that are not most recent dates into 'old' folder
+    if use_date != "":
         for j in files_i:
             if use_date not in j:
-                os.rename(j, 'old/%s'%j)  
+                os.rename(j, 'old/%s'%j)
                 #print('old/%s'%j)
---
+++ tools/sync_googledrive.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+check_update_py=/Volumes/Huitian/Projects/CD8_DEV_SC/tools/check_up_to_date.py
+
+git_repo_dir=/Volumes/Huitian/Projects/CD8_DEV_SC
+google_dir=/Volumes/Huitian/GSuite_Scripps/x_Active_projects/CD8_DEV_SC
+
+git_repo_figures=${git_repo_dir}/9_Figures
+git_repo_updateFigures=${git_repo_dir}/z_update_figures
+git_repo_updateManuscript=${git_repo_dir}/z_update_manuscript
+git_repo_updateSlides=${git_repo_dir}/z_update_slides
+
+google_figures=${google_dir}/9_Figures
+google_updateFigures=${google_dir}/z_update_figures
+google_updateManuscript=${google_dir}/z_update_manuscript
+google_updateSlides=${google_dir}/z_update_slides
+
+cd $git_repo_figures
+cp -r * $google_figures
+
+cd $git_repo_updateFigures
+cp -r * $google_updateFigures
+
+cd $git_repo_updateManuscript
+cp -r * $google_updateManuscript
+
+cd $git_repo_updateSlides
+cp -r * $google_updateSlides
+
+python $check_update_py $git_repo_updateFigures
+python $check_update_py $google_updateFigures
